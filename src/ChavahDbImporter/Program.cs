6da218108502273c651abb1282aa03513c3929b3
kdcllc <info@kingdavidconsulting.com>
2019-02-01 13:21:27 -0500

    bones of the project added

---
+++ src/ChavahDbImporter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ChavahDbImporter
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var host = new HostBuilder()
+                 .ConfigureHostConfiguration(configHost =>
+                 {
+                     configHost.SetBasePath(Directory.GetCurrentDirectory());
+                     configHost.AddJsonFile("hostsettings.json", optional: true);
+                     configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                     configHost.AddCommandLine(args);
+                 })
+                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 {
+                     configApp.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                     configApp.AddJsonFile(
+                         $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                         optional: true);
+                     configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                     configApp.AddCommandLine(args);
+                 })
+                 .ConfigureLogging((hostContext, configLogging) =>
+                 {
+                     configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                     configLogging.AddConsole();
+                     configLogging.AddDebug();
+                 })
+                 .UseConsoleLifetime()
+                 .Build();
+
+            var srv = host.Services;
+
+            using (host)
+            {
+                await host.RunAsync();
+
+                await host.WaitForShutdownAsync();
+            }
+        }
+
+    }
+}
